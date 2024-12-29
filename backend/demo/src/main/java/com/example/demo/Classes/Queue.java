f877285fa03ffea81a1b5d61d1c0466d4ae54291
abdonaware <abdonawar1234567@gmail.com>
2024-12-30 00:40:08 +0200

    Rename Mashine to Machine and update references; add CreateController and CreateService for machine and queue management

--- backend/demo/src/main/java/com/example/demo/Classes/Queue.java
+++ backend/demo/src/main/java/com/example/demo/Classes/Queue.java
@@ -1,52 +1,62 @@
 package com.example.demo.Classes;
+
 import java.util.List;
 
 import com.example.demo.DesginPattern.Observer;
 
 import lombok.Getter;
 import lombok.Setter;
+
 @Setter
 @Getter
 public class Queue implements Observer {
-    private List<Mashine> inMashines;
-    private List<Mashine> outMashines;
+    private List<Machine> inMashines;
+    private List<Machine> outMashines;
     private List<Products> products;
     private int noOfProducts;
 
-    public Queue() { }  
-    
+    public Queue() {
+    }
+
     @Override
     public void notifyall() {
-       for(Mashine m : outMashines){
-           m.setPendingProduct(true);
-       }
+        for (Machine m : outMashines) {
+            m.setPendingProduct(true);
+        }
     }
-    public void addProduct(Products p){
+
+    public void addProduct(Products p) {
         products.add(p);
         noOfProducts++;
     }
-    public void removeProduct(Products p){
+
+    public void removeProduct(Products p) {
         products.remove(p);
         noOfProducts--;
     }
-    public void addMashine(Mashine m){
+
+    public void addMashine(Machine m) {
         inMashines.add(m);
     }
-    public void removeMashine(Mashine m){
+
+    public void removeMashine(Machine m) {
         inMashines.remove(m);
     }
-    public void addOutMashine(Mashine m){
+
+    public void addOutMashine(Machine m) {
         outMashines.add(m);
     }
-    public void removeOutMashine(Mashine m){
+
+    public void removeOutMashine(Machine m) {
         outMashines.remove(m);
     }
-    public void processProduct(){
-        if(noOfProducts>0){
-            for(Mashine m : inMashines){
-                if(!m.isBusy()){
+
+    public void processProduct() {
+        if (noOfProducts > 0) {
+            for (Machine m : inMashines) {
+                if (!m.isBusy()) {
                     m.setBusy(true);
-                    if(!products.isEmpty()){
+                    if (!products.isEmpty()) {
                         m.setPendingProduct(true);
                     }
                     products.remove(0);
